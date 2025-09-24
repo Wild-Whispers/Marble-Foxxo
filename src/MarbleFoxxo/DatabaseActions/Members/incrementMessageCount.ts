be7c9f52d3b1246e3f1a66e0c3cedc872a2cc41c
Caleb Hesse <98936384+QuietWindUponTheMoor@users.noreply.github.com>
2025-09-23 19:29:25 -0500

    refactor: Adjust shard increment on message increment

--- src/MarbleFoxxo/DatabaseActions/Members/incrementMessageCount.ts
+++ src/MarbleFoxxo/DatabaseActions/Members/incrementMessageCount.ts
@@ -9,7 +9,22 @@ const func = {
         const user = await member.user;
 
         // Define shards per message
-        const shardsPerMessage = 2;
+        const defaultShardsPerMessage = 2;
+        const shardsPer1000Chars = 5;
+
+        // Get message content
+        const content = message.content;
+
+        let shardsToAward = defaultShardsPerMessage;
+        if (content && content.length > 0) {
+            const len = content.length;
+
+            if (len > 100 && len < 1000) shardsToAward = 3;
+            if (len >= 1000 && len < 2000) shardsToAward = shardsPer1000Chars;
+            if (len >= 2000 && len < 3000) shardsToAward = shardsPer1000Chars * 2;
+            if (len >= 3000 && len < 4000) shardsToAward = shardsPer1000Chars * 3;
+            if (len >= 4000) shardsToAward = shardsPer1000Chars * 4; // Nitro limit
+        }
 
         await mongo.database
             .collection("guild-members")
@@ -18,15 +33,15 @@ const func = {
                 [
                     {
                         $set: {
-                        // always increment msgsSent
+                        // Always increment msgsSent
                         msgsSent: { $add: ["$msgsSent", 1] },
 
-                        // if totalShards exists, increment it
-                        // else seed it with 50 and then add increment
+                        // If totalShards exists, increment it
+                        // Else seed it with 50 and then add increment
                         totalShards: {
                             $add: [
-                            { $ifNull: ["$totalShards", 50] },
-                            shardsPerMessage
+                                { $ifNull: ["$totalShards", 50] },
+                                shardsToAward
                             ]
                         },
 
