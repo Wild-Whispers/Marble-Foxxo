96d8b32ecfac07e7aa17f4d85362affbac5907c6
Caleb Hesse <98936384+QuietWindUponTheMoor@users.noreply.github.com>
2025-09-27 18:53:31 -0500

    feat: warnMember db action

---
+++ src/MarbleFoxxo/DatabaseActions/Moderation/warnMember.ts
@@ -0,0 +1,23 @@
+import { ModerationActions } from "@/_Enums/ModerationActions";
+import { getMongo } from "@/lib/mongo";
+import { GuildMember } from "discord.js";
+import { Actions } from "../Actions";
+
+const func = {
+    async warnMember(member: GuildMember, reason?: string) {
+        const mongo = getMongo();
+
+        await mongo.database
+            .collection("mod-action-logs")
+            .insertOne({
+                userID: member.user.id,
+                guildID: member.guild.id,
+                type: ModerationActions.WARN,
+                reason: reason ?? "No reason given"
+            });
+
+        return await Actions.incrementWarns(member);
+    }
+};
+
+export default func;
