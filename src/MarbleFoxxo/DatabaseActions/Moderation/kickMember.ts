38b59bdf0ca69d78325bf4c71fcbb5cc04426331
Caleb Hesse <98936384+QuietWindUponTheMoor@users.noreply.github.com>
2025-09-27 18:55:13 -0500

    feat: kickMember db action

---
+++ src/MarbleFoxxo/DatabaseActions/Moderation/kickMember.ts
@@ -0,0 +1,23 @@
+import { ModerationActions } from "@/_Enums/ModerationActions";
+import { getMongo } from "@/lib/mongo";
+import { GuildMember } from "discord.js";
+import { Actions } from "../Actions";
+
+const func = {
+    async kickMember(member: GuildMember, reason?: string) {
+        const mongo = getMongo();
+
+        await mongo.database
+            .collection("mod-action-logs")
+            .insertOne({
+                userID: member.user.id,
+                guildID: member.guild.id,
+                type: ModerationActions.KICK,
+                reason: reason ?? "No reason given"
+            });
+
+        return await Actions.incrementKicks(member);
+    }
+};
+
+export default func;
