0b7304138a6869b0b3d4dbb5d6a69ff1d817648d
Caleb Hesse <98936384+QuietWindUponTheMoor@users.noreply.github.com>
2025-09-27 12:06:33 -0500

    feat: Member join log channel comand

---
+++ src/MarbleFoxxo/Discord/Commands/Moderation/setMemberJoinChannel.ts
@@ -0,0 +1,32 @@
+import { Actions } from "@/MarbleFoxxo/DatabaseActions/Actions";
+import { ChatInputCommandInteraction, MessageFlags, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
+
+const name = "set-member-join-channel";
+const description = "Set the channel where member join logs are sent.";
+
+const command = {
+    data: new SlashCommandBuilder()
+        .setName(name)
+        .setDescription(description)
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+        .addChannelOption(option =>
+            option
+                .setName("channel")
+                .setDescription("The channel where member join logs are sent.")
+                .setRequired(true)
+        ),
+            
+    async execute(interaction: ChatInputCommandInteraction): Promise<void> {
+        const channel = interaction.options.getChannel("channel", true);
+
+        // Update moderation log channel
+        await Actions.setMemberJoinLogChannel(interaction.guild, channel.id);
+        
+        await interaction.reply({
+            content: `✅ Set <#${channel.id}> as the member join log channel.`,
+            flags: MessageFlags.Ephemeral
+        });
+    }
+};
+
+export default command;
