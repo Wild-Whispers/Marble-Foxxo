a0b96c3fe6398d399c305e6b57c18276d83d3de1
Caleb Hesse <98936384+QuietWindUponTheMoor@users.noreply.github.com>
2025-11-02 01:08:02 -0500

    feat: dashboard sidebar nav buttons active status

--- src/app/dashboard/DashboardNavButton.tsx
+++ src/app/dashboard/DashboardNavButton.tsx
@@ -1,7 +1,34 @@
 import Link from "next/link";
 import { ReactNode } from "react";
 
-export default function DashboardNavButton({ href, content }: { href: string, content: ReactNode }) {
+export default function DashboardNavButton({ href, content, isActive }: { href: string, content: ReactNode, isActive: boolean }) {
+    if (isActive) return (
+        <Link
+            href={href}
+            className="
+                flex
+                flex-row
+                justify-start
+                items-center
+                p-2
+
+                rounded-sm
+                bg-gradient-to-r
+                from-teal-700/50
+                via-teal-50/1
+                to-teal-50/1
+
+                hover:from-teal-400/50
+                hover:via-teal-50/1
+                hover:to-teal-50/1
+
+                gap-2
+            "
+        >
+            {content}
+        </Link>
+    );
+
     return <Link
         href={href}
         className="
@@ -14,8 +41,9 @@ export default function DashboardNavButton({ href, content }: { href: string, co
             rounded-sm
             hover:bg-gradient-to-r
             from-teal-400/50
-            via-teal-50/5
-            to-teal-50/5
+            via-teal-50/1
+            to-teal-50/1
+
             gap-2
         "
     >
--- src/app/dashboard/DashboardSidebar.tsx
+++ src/app/dashboard/DashboardSidebar.tsx
@@ -6,11 +6,12 @@ import { Cog6ToothIcon, HashtagIcon, InformationCircleIcon, ShieldExclamationIco
 import GuildsDropdown from "./GuildsDropdown";
 import DashboardNavButton from "./DashboardNavButton";
 import { useState } from "react";
-import { redirect } from "next/navigation";
+import { redirect, usePathname } from "next/navigation";
 
 /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
 export default function DashboardSidebar({ accessToken, guilds }: { accessToken: string | null | undefined, guilds: Array<any> | null | undefined }) {
     const [guildParam, setGuildParam] = useState<string>(guilds && guilds.length > 0 ? guilds[0].id : "null");
+    const pathname = usePathname();
 
     const setWorkingGuild = (guildID: string) => {
         setGuildParam(guildID);
@@ -19,9 +20,7 @@ export default function DashboardSidebar({ accessToken, guilds }: { accessToken:
     };
 
     if (!accessToken) return <ErrorMessage description="Your session has expired. Please sign in again." />;
-
     if (!guilds) return <ErrorMessage description="There was an error fetching your guilds." />;
-
     if (accessToken && guilds.length === 0) return <ErrorMessage description="Your share no guilds with Marble Foxxo." />;
 
     const navLinks = [
@@ -33,14 +32,27 @@ export default function DashboardSidebar({ accessToken, guilds }: { accessToken:
     ];
 
     return (
-        <Col id="dashboard-sidebar-content">
+        <Col id="dashboard-sidebar-content" classes="gap-2">
             <GuildsDropdown guilds={guilds} setWorkingGuild={setWorkingGuild} />
 
             <Col id="dashboard-sidebar-navigation-links">
                 {
                     navLinks.map((link: any, i: number) => { /* eslint-disable-line @typescript-eslint/no-explicit-any */
+                        const cleanHref = link.href.split("?")[0];
+
+                        let isActive;
+                        if (cleanHref === "/dashboard") {
+                            isActive = pathname === "/dashboard";
+                        } else {
+                            isActive = pathname.startsWith(cleanHref);
+                        }
+
                         return (
-                            <DashboardNavButton key={i} href={link.href} content={
+                            <DashboardNavButton
+                                key={i}
+                                href={link.href}
+                                isActive={isActive}
+                                content={
                                     <>
                                         {link.icon}
                                         <p>{link.label}</p>
